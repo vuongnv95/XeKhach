f470271588439cbea355a6b54b26fda853aab699
CuongVV <CuongVV@rikkeisoft.com>
2019-03-14 15:45:00 +0700

    Initial commit

---
+++ app/src/main/java/jp/app/xekhach/MainActivity.kt
@@ -0,0 +1,12 @@
+package jp.app.xekhach
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
